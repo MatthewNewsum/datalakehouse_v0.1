b01eca8ece164b4ee4c56132054c6943d4b7cce4
Matthew Newsum <6485858+MatthewNewsum@users.noreply.github.com>
2025-01-13 12:56:48 -0500

    updated the iam arn interpolation syntax

--- iam.tf
+++ iam.tf
@@ -32,20 +32,46 @@ resource "aws_iam_role_policy" "glue_s3" {
         Effect = "Allow"
         Action = [
           "s3:GetObject",
-          "s3:PutObject",
+          "s3:PutObject", 
           "s3:DeleteObject",
           "s3:ListBucket"
         ]
         Resource = [
           aws_s3_bucket.raw_zone.arn,
           "${aws_s3_bucket.raw_zone.arn}/*",
-          aws_s3_bucket.processed_zone.arn,
+          aws_s3_bucket.processed_zone.arn, # Fixed: Removed quotes
           "${aws_s3_bucket.processed_zone.arn}/*"
         ]
       }
     ]
   })
 }
+
+# resource "aws_iam_role_policy" "glue_s3" {
+#   name = "GlueS3Policy"
+#   role = aws_iam_role.glue_role.id
+
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "s3:GetObject",
+#           "s3:PutObject",
+#           "s3:DeleteObject",
+#           "s3:ListBucket"
+#         ]
+#         Resource = [
+#           aws_s3_bucket.raw_zone.arn,
+#           "${aws_s3_bucket.raw_zone.arn}/*",
+#           "aws_s3_bucket.processed_zone.arn",
+#           "${aws_s3_bucket.processed_zone.arn}/*"
+#         ]
+#       }
+#     ]
+#   })
+# }
 
 # Additional S3 access policy for Glue
 resource "aws_iam_role_policy" "glue_s3_policy" {
